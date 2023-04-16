0c6361f7d68c20b7b1e8071621fc75f990cefe28
Víctor Ceballos Espinosa <victcebesp@gmail.com>
2023-04-16 22:32:11 +0200

    extract common function validteOrder and check destination country agains db

--- src/handler/createOrder.js
+++ src/handler/createOrder.js
@@ -1,6 +1,6 @@
 const { buildSuccessResponse, buildFailureResponse } = require('../utils/responseBuilder')
 const { createOrder } = require('../service/orders')
-const { validDestinationCountry, validImportType } = require('../utils/validators')
+const { validateOrder } = require('../utils/validators')
 const jwt = require('jsonwebtoken')
 
 module.exports.handler = async (event) => {
@@ -12,13 +12,4 @@ module.exports.handler = async (event) => {
     .then(newOrder => createOrder(sub, newOrder))
     .then((createdOrder) => buildSuccessResponse(200, createdOrder))
     .catch(error => buildFailureResponse(500, error.message))
-}
-
-const validateOrder = async (eventBody) => {
-  const newOrder = JSON.parse(eventBody)
-  if (!newOrder.sku || newOrder.sku.length === 0) throw new Error('sku attribute cannot be empty')
-  if (!newOrder.unitsAmount || newOrder.unitsAmount <= 0) throw new Error('Amount of units must be greater than zero')
-  if (!validDestinationCountry(newOrder.destinationCountry)) throw new Error('Invalid destination country')
-  if (!validImportType(newOrder.importType)) throw new Error('Invalid import type')
-  return newOrder
 }
