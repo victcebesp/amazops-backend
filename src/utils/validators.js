c43828afea464cd32f4655ce006a225918a31f86
Víctor Ceballos Espinosa <victcebesp@gmail.com>
2023-04-16 23:21:34 +0200

    fix bug, wait for validate destination country function

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -4,7 +4,7 @@ const validateOrder = async (eventBody) => {
   const newOrder = JSON.parse(eventBody)
   if (!newOrder.sku || newOrder.sku.length === 0) throw new Error('sku attribute cannot be empty')
   if (!newOrder.unitsAmount || newOrder.unitsAmount <= 0) throw new Error('Amount of units must be greater than zero')
-  if (!validDestinationCountry(newOrder.destinationCountry)) throw new Error('Invalid destination country')
+  if (!await validDestinationCountry(newOrder.destinationCountry)) throw new Error('Invalid destination country')
   if (!validImportType(newOrder.importType)) throw new Error('Invalid import type')
   return newOrder
 }
