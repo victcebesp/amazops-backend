92d1510a1c36a84668b1b669ad525cd60ba6804f
Víctor Ceballos Espinosa <victcebesp@gmail.com>
2023-04-17 18:08:45 +0200

    delete order events when order gets deleted

--- src/handler/deleteOrder.js
+++ src/handler/deleteOrder.js
@@ -1,5 +1,5 @@
 const { buildSuccessResponse, buildFailureResponse } = require('../utils/responseBuilder')
-const { deleteOrder } = require('../service/orders')
+const { deleteOrder, deleteOrderEvents } = require('../service/orders')
 const jwt = require('jsonwebtoken')
 
 module.exports.handler = async (event) => {
@@ -8,6 +8,7 @@ module.exports.handler = async (event) => {
   const decodedToken = jwt.decode(token, { complete: true })
   const sub = decodedToken.payload.sub
   return deleteOrder(sub, event.pathParameters.orderId)
+    .then(() => deleteOrderEvents(sub, event.pathParameters.orderId))
     .then(() => buildSuccessResponse(200))
     .catch(error => buildFailureResponse(404, error.message))
 }
